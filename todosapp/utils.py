6a453e9c3272b21c90f62aaa4243c8c10c2b6dcc
Abdalmohsen-H <egywebdes@gmail.com>
2023-08-16 21:24:35 +0300

    remove test code on core serilaizer

--- todosapp/utils.py
+++ todosapp/utils.py
@@ -20,9 +20,7 @@ def task_serializer(tasks):
     """Serialize tasks to json
     argument : tasks must be iterable, ideally a list
     """
-    serialized_data = serializers.serialize(
-        "json", tasks, fields=["title", "created_at", "updated_at"]
-    )
+    serialized_data = serializers.serialize("json", tasks)
     return json.loads(serialized_data)
 
 
